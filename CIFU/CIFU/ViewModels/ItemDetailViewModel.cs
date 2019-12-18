7208b519764824892906c5db25630b41995a8a6b
safonov <safonov1985@protonmail.com>
2019-12-18 21:51:27 +0100

    First Update

---
+++ CIFU/CIFU/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using CIFU.Models;
+
+namespace CIFU.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
