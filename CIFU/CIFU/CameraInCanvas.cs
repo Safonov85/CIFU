c93a6183d4eb661603f6364486d9c452f8d9a0eb
safonov <safonov1985@protonmail.com>
2020-01-04 19:46:41 +0100

    Added a camera class for future canvas

---
+++ CIFU/CIFU/CameraInCanvas.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CIFU
+{
+    public class CameraInCanvas
+    {
+        public CameraInCanvas()
+        {
+            // constructor loading on object
+        }
+    }
+}
